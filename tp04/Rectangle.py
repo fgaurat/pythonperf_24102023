0c37fa45fd312f1f9a2886ff6d6b5c3f428dbf3f
Frédéric GAURAT <fgaurat@gmail.com>
2023-10-25 11:30:09 +0200

    dataclass

--- tp04/Rectangle.py
+++ tp04/Rectangle.py
@@ -2,10 +2,18 @@
 
 
 class Rectangle:
+    __cpt = 0
 
-    def __init__(self,longueur,largeur) -> None:
+    def __init__(self,longueur=1,largeur=1) -> None:
         self.__longueur = longueur
         self.__largeur= largeur
+        Rectangle.__cpt+=1
+        
+    @classmethod
+    def build_from_str(cls,value=""):
+        longueur,largeur = [int(v) for v in value.split("x")]
+        return cls(longueur,largeur )
+
 
     @property
     def longueur(self):
@@ -31,13 +39,20 @@ class Rectangle:
     @property
     def surface(self):
         return self.__longueur*self.__largeur
-    
+
+    @staticmethod
+    def get_cpt():
+        return Rectangle.__cpt
+
     def __del__(self):
         print("Rectangle def __del__(self)")
+        Rectangle.__cpt-=1
 
     def __str__(self) -> str:
         return f"{__class__.__name__} {self.__longueur=},{self.__largeur=}"
     
+    def __eq__(self, __value: object) -> bool:
+        return self.__longueur == __value.__longueur and self.__largeur == __value.__largeur
     
     # longueur = property(get_longueur,set_longueur,doc="propriété longueur")
     # largeur = property(get_largeur,set_largeur,doc="propriété largeur")
--- tp04/main.py
+++ tp04/main.py
@@ -1,4 +1,5 @@
 from Rectangle import Rectangle
+from DataRectangle import DataRectangle
 
 def main():
     r = Rectangle(6,4)
@@ -17,5 +18,27 @@ def main():
     s = str(r)
     print(s)
 
+    r2 = Rectangle(6,4)
+    r3 = Rectangle(6,4)
+    print("cpt r1",r1.get_cpt())
+    print("cpt",Rectangle.get_cpt())
+    
+    # if r2.__eq__(r3)
+    if r2 == r3:
+        print("ok")
+    else:
+        print("ko")
+
+    print(50*'-')
+    r4 = Rectangle()
+    print(r4)
+    r5 = Rectangle.build_from_str("6x3")
+    print(r5)
+    print(50*'-')
+    r6 = DataRectangle(2,3)
+    print(r6)
+    print(r6.surface)
+    print()
+    print()
 if __name__ == '__main__':
     main()
